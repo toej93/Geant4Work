5fbfb451eaa6b211db4a5a3bb0af526612880a53
Jorge Torres <jorge.torresespinosa@yale.edu>
2021-11-10 08:08:00 -0500

    Adding latest work on muons. Still need to document and cleanup

---
+++ muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/DependInfo.cmake
@@ -0,0 +1,69 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/muonsInPanel.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/muonsInPanel.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/DetectorConstruction.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/DetectorConstruction.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/DetectorMessenger.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/DetectorMessenger.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/EventAction.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/EventAction.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/HistoManager.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/HistoManager.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/PhysListEmLivermore.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/PhysListEmLivermore.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/PhysListEmPenelope.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/PhysListEmPenelope.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/PhysListEmStandard.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/PhysListEmStandard.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/PhysicsList.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/PhysicsList.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/PhysicsListMessenger.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/PhysicsListMessenger.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/PrimaryGeneratorAction.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/PrimaryGeneratorAction.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/RunAction.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/RunAction.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/StackingAction.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/StackingAction.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/StackingMessenger.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/StackingMessenger.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/StepMax.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/StepMax.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/StepMaxMessenger.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/StepMaxMessenger.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/SteppingAction.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/SteppingAction.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/SteppingVerbose.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/SteppingVerbose.cc.o"
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/src/TrackingAction.cc" "/home/jat89/Software/Geant4Work/muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/src/TrackingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/jat89/Software/Geant4Work/muons/muonsInPanel/include"
+  "/home/jat89/Software/MC/geant4-install/10.07/include/Geant4"
+  "/home/jat89/Software/MC/geant4-install/10.07/include"
+  "/gpfs/loomis/apps/avx/software/expat/2.2.9-GCCcore-10.2.0/include"
+  "/gpfs/loomis/apps/avx/software/libglvnd/1.3.2-GCCcore-10.2.0/include"
+  "/usr/include/qt5"
+  "/usr/include/qt5/QtCore"
+  "/usr/lib64/qt5/./mkspecs/linux-g++"
+  "/usr/include/qt5/QtGui"
+  "/usr/include/qt5/QtWidgets"
+  "/usr/include/qt5/QtOpenGL"
+  "/usr/include/qt5/QtPrintSupport"
+  "/gpfs/loomis/apps/avx/software/X11/20201008-GCCcore-10.2.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ muons/muonsInPanel_build/CMakeFiles/muonsInPanel.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/muonsInPanel.dir/muonsInPanel.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/DetectorMessenger.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/EventAction.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/HistoManager.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/PhysListEmLivermore.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/PhysListEmPenelope.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/PhysListEmStandard.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/PhysicsListMessenger.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/RunAction.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/StackingAction.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/StackingMessenger.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/StepMax.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/StepMaxMessenger.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/SteppingAction.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/SteppingVerbose.cc.o"
+  "CMakeFiles/muonsInPanel.dir/src/TrackingAction.cc.o"
+  "muonsInPanel"
+  "muonsInPanel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/muonsInPanel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
